6ed1f803e255586644ceb63dcf50dde0838c3083
ADMIN\Administrator <tien.nguyen20020713@gmail.com>
2024-11-23 20:09:44 +0700

    Final update

---
+++ Lab3/Core/Inc/auto.h
@@ -0,0 +1,18 @@
+/*
+ * auto.h
+ *
+ *  Created on: Nov 12, 2024
+ *      Author: Administrator
+ */
+
+#ifndef INC_AUTO_H_
+#define INC_AUTO_H_
+
+#include "global.h"
+#include "display_led.h"
+#include "software_timer.h"
+#include "button.h"
+
+void autoRun();
+
+#endif /* INC_AUTO_H_ */
---
+++ Lab3/Core/Inc/manual.h
@@ -0,0 +1,17 @@
+/*
+ * manual.h
+ *
+ *  Created on: Nov 12, 2024
+ *      Author: Administrator
+ */
+
+#ifndef INC_MANUAL_H_
+#define INC_MANUAL_H_
+
+#include "global.h"
+#include "display_led.h"
+#include "button.h"
+
+void manualRun();
+
+#endif /* INC_MANUAL_H_ */
